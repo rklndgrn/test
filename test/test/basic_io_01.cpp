b5e6891a463c31e408ae7fea0625504fe19b6714
Erik L <rk.lndgrn@gmail.com>
2018-11-05 14:13:48 +0100

    Lab Intro 01 from TDDC76

---
+++ test/test/basic_io_01.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	char exit;
+	cout << "Skriv in ett heltal: ";
+	cin >> number;
+	cout << "Du skrev in talet " << number << endl;
+	
+	cin.ignore(1000, '\n');
+	cout << "Skriv in fem heltal: ";
+	cin >> number;
+	cout << "Du skrev in talen: " << number;
+	cin >> number;
+	cout << " " << number;
+	cin >> number;
+	cout << " " << number;
+	cin >> number;
+	cout << " " << number;
+	cin >> number;
+	cout << " " << number << endl;
+
+	double decimal;
+	cin.ignore(1000, '\n');
+	cout << "Skriv in ett heltal och ett flyttal: ";
+	cin >> number;
+	cin >> decimal;
+	cout << "Du skrev in heltalet:" << setw(10) << right << number << endl;
+	cout << "Du skrev in flyttalet:" << setw(9) << fixed << setprecision(4) << decimal << endl;
+
+	cin.ignore(1000, '\n');
+	cout << "Skriv in ett flyttal och ett heltal: ";
+	cin >> decimal;
+	cin >> number;
+	cout << setfill('-');
+	cout << "Du skrev in heltalet:" << setw(10) << number << endl;
+	cout << "Du skrev in flyttalet:" << setw(9) << decimal << endl;
+
+	char c;
+	cin.ignore(1000, '\n');
+	cout << "Skriv in ett tecken: ";
+	cin >> c;
+	cout << "Du skrev in tecknet: " << c << endl;
+
+	string str;
+	cin.ignore(1000, '\n');
+	cout << "Skriv in ett ord: ";
+	cin >> str;
+	cout << "Du skrev in ordet: " << str << endl;
+
+	cin.ignore(1000, '\n');
+	cout << "Skriv in ett heltal och ett ord: ";
+	cin >> number;
+	cin >> str;
+	cout << "Du skrev in talet |" << number << "|";
+	cout << " och ordet |" << str << "|" << endl;
+
+	cin.ignore(1000, '\n');
+	cout << "Skriv in ett tecken och ett ord: ";
+	cin >> c;
+	cin >> str;
+	cout << "Du skrev in \"" << str << "\"";
+	cout << " och \'" << c << "\'" << endl;
+
+	cin.ignore(1000, '\n');
+	cout << "Skriv in en rad text: ";
+	getline(cin, str);
+	cout << "Du skrev in: \"" << str << "\"" << endl;
+
+	cout << "Skriv in en till rad text: ";
+	getline(cin, str);
+	cout << "Du skrev in: \"" << str << "\"" << endl;
+
+	cout << "Skriv in tre ord: ";
+	cin >> str;
+	cout << "Du skrev in: \"" << str << "\"";
+	cin >> str;
+	cout << ", \"" << str << "\"";
+	cin >> str;
+	cout << " och \"" << str << "\"" << endl;
+
+
+	cin >> exit;
+
+	return 0;
+}
