c0853b671e46705c5e23adc47bf8a40264ca0840
Andreas Andersson <andan879@student.liu.se>
2018-11-06 09:05:21 +0100

    La till lite kickass-kod igen

--- test/test/Tasks.cpp
+++ test/test/Tasks.cpp
@@ -9,7 +9,7 @@ void StartTask01()
 	int number;
 	char exit;
 
-	cout << "Här är lite kickass-code, right? [y=1/0=n]: ";
+	cout << "Här är lite kickass-code, right????? [y=1/0=n]: ";
 	cin >> number;
 	cout << "Oavsett vad du svarade har du fel " << number << endl;
 
